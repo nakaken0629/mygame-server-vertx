5e0ae9663b45f73383bb199872781c9c114839b2
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-04-13 01:03:20 +0900

    stompサーバーの作成

--- src/main/java/red/itvirtuoso/mygame/MainVerticle.java
+++ src/main/java/red/itvirtuoso/mygame/MainVerticle.java
@@ -1,14 +1,41 @@
 package red.itvirtuoso.mygame;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.stomp.Destination;
+import io.vertx.ext.stomp.StompServer;
+import io.vertx.ext.stomp.StompServerHandler;
 
 public class MainVerticle extends AbstractVerticle {
+  private StompServer server;
 
   @Override
   public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello MyGame Vert.x!"))
-        .listen(8080);
+    server = StompServer.create(vertx)
+      .handler(StompServerHandler.create(vertx)
+        .destinationFactory((v, name) -> {
+          if (name.startsWith("/queue")) {
+            return Destination.queue(vertx, name);
+          } else {
+            return Destination.topic(vertx, name);
+          }
+        }))
+      .listen(1234, "0.0.0.0", ar -> {
+        if (ar.failed()) {
+          System.out.println("Failing to start the STOMP server : " + ar.cause().getMessage());
+        } else {
+          System.out.println("Ready to receive STOMP frames");
+        }
+      });
   }
 
+  @Override
+  public void stop() throws Exception {
+    server.close(ar -> {
+      if (ar.succeeded()) {
+        System.out.println("The STOMP server has been closed");
+      } else {
+        System.out.println("The STOMP server failed to close : " + ar.cause().getMessage());
+      }
+    });
+  }
 }
